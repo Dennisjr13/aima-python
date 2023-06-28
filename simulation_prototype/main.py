2682451d767c5f4fd6b2952843e2c85f2fce1c3e
alexzgu <alexanderzgu@gmail.com>
2023-06-27 23:45:49 -0400

    updated 1 comment

--- simulation_prototype/main.py
+++ simulation_prototype/main.py
@@ -12,7 +12,7 @@ Timer functionality added. SLAM added.
 
 def main():
     obstacles = list_of_obstacles
-    goal = (400, 400)  # Tuple
+    goal = (400, 400)  # Coordinates of the goal (what the agent is looking for).
     env = Environment((500, 500), obstacles, goal)
     agent = Agent(env, (250, 250))  # Starting at the center of the screen
     sim = Simulation(env, agent)
