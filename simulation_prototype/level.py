2346a4c118369d361b87a5ff0c60744b51055b1c
alexzgu <alexanderzgu@gmail.com>
2023-07-08 17:50:57 -0400

    level parsing update
    organized level data into separate folders "evaluation_levels" and "misc_levels"

--- simulation_prototype/level.py
+++ simulation_prototype/level.py
@@ -6,24 +6,40 @@ import os
 
 
 class JSONParser:
-    def __init__(self, filename):
-        if filename[-5:] == ".json":
-            filename = filename[:-5]
-        file_dir = os.getcwd() + f'\\levels\\{filename}.json'
+    def __init__(self, filename, folder="m"):
+        file_dir = self.get_file_dir(filename, folder)
+        # load file data
         with open(file_dir, 'r') as file:
             data = json.load(file)
             self.screen_size = tuple(data['screen_size'])
             self.agent_start = tuple(data['agent_start'])
             self.goal = tuple(data['goal'])
             self.obstacles = data['obstacles']
+        self.add_border_obstacles()
 
-            # Obstacles for the edge of the simulated space.
-            edge_width = 5  # edge border width
-            self.obstacles.append([-edge_width, 0, edge_width, self.screen_size[0]])
-            self.obstacles.append([0, -edge_width, self.screen_size[1], edge_width])
-            self.obstacles.append([self.screen_size[0], 0, edge_width, self.screen_size[1]])
-            self.obstacles.append([0, self.screen_size[1], self.screen_size[0], edge_width])
+    @staticmethod
+    def folder_parse(folder):
+        folder_name = ""
+        if folder == "m":
+            folder_name = "misc_levels\\"
+        elif folder == "e":
+            folder_name = "evaluation_levels\\"
+        return folder_name
 
+    def get_file_dir(self, filename, folder):
+        folder_name = self.folder_parse(folder)
+        if filename[-5:] == ".json":
+            filename = filename[:-5]
+        directory = os.getcwd() + f'\\levels\\{folder_name}{filename}.json'
+        return directory
+
+    def add_border_obstacles(self):
+        """Obstacles for the edge of the simulated space."""
+        edge_width = 5  # edge border width
+        self.obstacles.append([-edge_width, 0, edge_width, self.screen_size[0]])
+        self.obstacles.append([0, -edge_width, self.screen_size[1], edge_width])
+        self.obstacles.append([self.screen_size[0], 0, edge_width, self.screen_size[1]])
+        self.obstacles.append([0, self.screen_size[1], self.screen_size[0], edge_width])
 
 def main():
     file_name = 'new_level'
