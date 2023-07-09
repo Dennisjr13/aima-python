9f081b24208221601a5a3bd46ac4a9a967ef152e
Dennis Juhasz <dennisjr13@gmail.com>
2023-07-09 11:39:36 -0400

    add iteration limit to AStar

--- simulation_prototype/AStar_agent.py
+++ simulation_prototype/AStar_agent.py
@@ -18,11 +18,16 @@ class Node:
 
 class AStarAgent:
 
-    def __init__(self, sim):
+    def __init__(self, sim, allow_diagonal_movement=False):
         self.agent = sim.agent
         self.grid_map = sim.grid
-        #self.grid_map = GridMap(sim, 50, 50)
+        # self.grid_map = GridMap(sim, 50, 50)
         self.obstacles = sim.inflated_obstacles
+        self.max_iterations = self.grid_map.width * self.grid_map.height // 2
+
+        self.adjacent_nodes = ((0, -1), (0, 1), (-1, 0), (1, 0))
+        if allow_diagonal_movement:
+            self.adjacent_nodes = ((0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1))
 
         self.path_cost = 0
 
@@ -40,8 +45,15 @@ class AStarAgent:
         # Add the initial start node
         open_list.append(start_node)
 
-        # Loop until you find the end
+        # Loop until you find the end or reach max_iterations
+        iterations = 0
         while len(open_list) > 0:
+            iterations += 1
+
+            if iterations > self.max_iterations:
+                # return the current path so far
+                print("Too many iterations, unable to find solution")
+                return self.build_path(current_node)
 
             # Get the current node
             current_node = open_list[0]
@@ -57,31 +69,20 @@ class AStarAgent:
 
             # Found the goal
             if self.grid_map.is_goal(current_node.coordinates[0], current_node.coordinates[1]):
-                path = []
-                current = current_node
-                while current is not None:
-                    cur_coords = self.grid_map.get_center(current.coordinates[0],
-                                                          current.coordinates[1])
-                    path.append(cur_coords)
-                    if current.parent is not None:
-                        par_coords = self.grid_map.get_center(current.parent.coordinates[0],
-                                                              current.parent.coordinates[1])
-                        self.path_cost += dist((cur_coords[0], cur_coords[1]),
-                                               (par_coords[0], par_coords[1]))
-
-                    current = current.parent
-                return path[::-1]  # Return reversed path
+                return self.build_path(current_node)
 
             # Generate children with adjacent squares
             children = []
-            for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]:
+            for new_position in self.adjacent_nodes:
 
                 # Get node position
-                node_position = (current_node.coordinates[0] + new_position[0], current_node.coordinates[1] + new_position[1])
+                node_position = (
+                current_node.coordinates[0] + new_position[0], current_node.coordinates[1] + new_position[1])
 
                 # Make sure point is within range of canvas
                 canvas = self.agent.env.size
-                if node_position[0] > self.grid_map.width or node_position[0] < 0 or node_position[1] > self.grid_map.height or node_position[1] < 0:
+                if node_position[0] > self.grid_map.width or node_position[0] < 0 or node_position[
+                    1] > self.grid_map.height or node_position[1] < 0:
                     continue
 
                 # Make sure point is not in an obstacle
@@ -104,7 +105,8 @@ class AStarAgent:
 
                 # Create the f, g, and h values
                 child.g = current_node.g + 1
-                child.h = ((child.coordinates[0] - goal_node.coordinates[0]) ** 2) + ((child.coordinates[1] - goal_node.coordinates[1]) ** 2)
+                child.h = ((child.coordinates[0] - goal_node.coordinates[0]) ** 2) + (
+                            (child.coordinates[1] - goal_node.coordinates[1]) ** 2)
                 child.f = child.g + child.h
 
                 # Check if child is already on the open list
@@ -114,6 +116,25 @@ class AStarAgent:
 
                 # Add the child to the open list
                 open_list.append(child)
+                self.grid_map.set_cell_value(1, child.coordinates[0], child.coordinates[1])
+
+        print("Couldn't get a path to destination")
+
+    def build_path(self, last_node_added):
+        path = []
+        current = last_node_added
+        while current is not None:
+            cur_coords = self.grid_map.get_center(current.coordinates[0],
+                                                  current.coordinates[1])
+            path.append(cur_coords)
+            if current.parent is not None:
+                par_coords = self.grid_map.get_center(current.parent.coordinates[0],
+                                                      current.parent.coordinates[1])
+                self.path_cost += dist((cur_coords[0], cur_coords[1]),
+                                       (par_coords[0], par_coords[1]))
+
+            current = current.parent
+        return path[::-1]  # Return reversed path
 
 
 # for debugging purposes
